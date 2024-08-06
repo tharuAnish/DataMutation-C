7297c54c6838d12a34153d7c66a5023ae6d8d7cd
tharuAnish <namoanishtharu@gmail.com>
2024-08-06 16:05:37 +0530

    setting up form actions, creating server actions, using use form status hook

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,18 +1,21 @@
-import { storePost } from '@/lib/posts';
+import FormSubmit from "@/components/form-submit"
+import { storePost } from "@/lib/posts"
+import { redirect } from "next/navigation"
 
 export default function NewPostPage() {
   async function createPost(formData) {
-    "use server";
-    const title = formData.get('title');
-    const image = formData.get('image');
-    const content = formData.get('content');
+    "use server"
+    const title = formData.get("title")
+    const image = formData.get("image")
+    const content = formData.get("content")
 
-    storePost({
-      imageUrl: '',
+    await storePost({
+      imageUrl: "",
       title,
       content,
-      userId: 1
+      userId: 1,
     })
+    redirect("/feed")
   }
 
   return (
@@ -37,10 +40,9 @@ export default function NewPostPage() {
           <textarea id="content" name="content" rows="5" />
         </p>
         <p className="form-actions">
-          <button type="reset">Reset</button>
-          <button>Create Post</button>
+          <FormSubmit />
         </p>
       </form>
     </>
-  );
+  )
 }
