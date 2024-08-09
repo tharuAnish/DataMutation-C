9641457d92eaad4240b37bec55c1fd5d9b3e402d
tharuAnish <namoanishtharu@gmail.com>
2024-08-09 20:44:50 +0530

    configuring meta data

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,12 +1,21 @@
-import Posts from '@/components/posts';
-import { getPosts } from '@/lib/posts';
+import Posts from "@/components/posts"
+import { getPosts } from "@/lib/posts"
+
+export async function generateMetadata() {
+  const posts = await getPosts()
+  const numberOfPosts = posts.length
+  return {
+    title: `Browse all our ${numberOfPosts} posts`,
+    description: "Our Latest Posts",
+  }
+}
 
 export default async function FeedPage() {
-  const posts = await getPosts();
+  const posts = await getPosts()
   return (
     <>
       <h1>All posts by all users</h1>
       <Posts posts={posts} />
     </>
-  );
+  )
 }
